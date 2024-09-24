7f6ed117df5095e79733d33da0e8bca204833412
Mavafelly <matthewfeliciano16@gmail.com>
2024-09-24 11:13:23 -0400

    woking

---
+++ src/components/WeatherForecast.jsx
@@ -0,0 +1,12 @@
+const WeatherForecasts = (props) => {
+    return (
+        <div className="weather">
+            <h2>{props.day}</h2>
+            <img src={props.imgSrc} alt={props.imgAlt} />
+            <p><span>Conditions: </span>{props.conditions}</p>
+            <p><span>Time: </span>{props.timeOfDay}</p>
+        </div>
+    );
+};
+
+export default WeatherForecasts;
